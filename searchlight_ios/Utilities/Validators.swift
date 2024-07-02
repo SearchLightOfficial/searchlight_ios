2703ea1b133e4f11851f26a22d1e877113ce8d14
PleBea <bini060315kim@gmail.com>
2024-07-02 15:32:43 +0900

    fix: Change form action logic

---
+++ searchlight_ios/Utilities/Validators.swift
@@ -0,0 +1,14 @@
+//
+//  RegexValidator.swift
+//  searchlight_ios
+//
+//  Created by 김성빈 on 7/2/24.
+//
+
+import Foundation
+
+func 
+
+func validateByRegex(_ input: String, _ regex: String) -> Bool {
+    return true
+}
